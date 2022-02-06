019dc2684d02938e55ce27a2d395c75f6daf625c
Effiti <EffitiDEV@gmail.com>
2022-02-06 16:22:08 +0100

    write ParsedOptions::get

--- opts.cpp
+++ opts.cpp
@@ -1,3 +1,15 @@
 #include "opts.hpp"
+#include <variant>
 
-std::variant<std::sring, bool, std::monostate> ParsedOptions::get(std::string
+std::variant<std::string, bool, std::monostate> ParsedOptions::get(std::string key){
+  if(mValues.count(key)) {
+    if (std::holds_alternative<bool>(mValues[key]))
+      return std::get<bool>(mValues[key]);
+    else
+      return std::get<std::string>(mValues[key]);
+  }
+  else {
+    return std::monostate();
+  }
+
+}
