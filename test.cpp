889c249aa1379cf0978f1aceef71658e31e8f610
Effiti <EffitiDEV@gmail.com>
2022-03-05 18:04:01 +0100

    remove test.cpp

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include "common.hpp"
-#include "logging.hpp"
-
-int main(int argc, char **argv) {
-  logger::Logger l{5};
-  if (argc <= 1)
-    l.log(logger::messageType::EDGECASE, "No arguments provided!!", HERE);
-  return 0;
-}
